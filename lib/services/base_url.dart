14fd543e38f557aabf74d6c20e56f9274327cd9b
Sebastian1033 <111609725+kirito1033@users.noreply.github.com>
2025-10-02 17:58:28 -0500

    Se realiza carfa del api

--- lib/services/base_url.dart
+++ lib/services/base_url.dart
@@ -1,6 +1,6 @@
 class BaseUrlService {
 
-  static const String baseUrl = "https://52775c5b2b07.ngrok-free.app/api_v1";
+  static const String baseUrl = "https://innovatech-api.onrender.com/api_v1";
 
 
   static String getBaseUrl() {
